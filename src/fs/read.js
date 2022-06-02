e456e3a8d36dfc51581859455fffb4e7ffda8085
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-02 20:34:39 +0300

    feat: add fs-read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    fs.readFile(
+        path.join(__dirname, 'files', 'fileToRead.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) throw new Error('FS operation failed');
+            console.log(data);
+        }
+    );
 };
+
+read();
