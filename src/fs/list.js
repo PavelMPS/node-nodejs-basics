a633e4a2f08650a669a8fa141f00fbac2cf8848c
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-02 20:33:54 +0300

    feat: add fs-list solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const list = async () => {
-    // Write your code here 
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+        if (err) throw new Error('FS operation failed');
+        files.forEach(file => console.log(file.split('.')[0]));
+    }) 
 };
+
+list();
