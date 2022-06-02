674a9f1de64b525664abb6e8d3f5fbac15260a12
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-02 20:35:21 +0300

    feat: add fs-rename solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,20 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathFilesDirectory = path.join(__dirname, 'files');
+
 export const rename = async () => {
-    // Write your code here 
+    fs.rename(
+        path.join(pathFilesDirectory, 'wrongFilename.txt'),
+        path.join(pathFilesDirectory, 'wrongFilename.md'),
+        err => {
+            if (err) throw new Error('FS operation failed');
+        }
+    ); 
 };
+
+rename();
