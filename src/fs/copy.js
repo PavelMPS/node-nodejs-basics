5e0c0feeccd9bb5e88f2c5beb0f546af57800bd2
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-02 20:31:39 +0300

    feat: add fs-copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,31 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathBaseFolder = path.join(__dirname, "files");
+const pathCopyFolder = path.join(__dirname, "files-copy");
+
 export const copy = async () => {
-    // Write your code here 
+  fs.mkdir(pathCopyFolder, { recursive: true }, err => {
+    if (err) throw new Error('FS operation failed');
+  });
+
+  fs.readdir(pathBaseFolder, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.copyFile(
+        path.join(pathBaseFolder, file),
+        path.join(pathCopyFolder, file),
+        err => {
+            if (err) throw new Error('FS operation failed');
+        }
+      );
+    });
+  });
 };
+
+copy();
