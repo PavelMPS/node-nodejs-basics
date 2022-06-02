b3b2afd2a499e6b3cebc8f8664b525d01fd4bc6b
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-02 23:31:44 +0300

    feat: add streams-write solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,22 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import * as readline from 'node:readline';
+import { stdin as input, stdout as output } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const readLine = readline.createInterface({ input, output });
+const myWriteStream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'),'utf-8');
+
 export const write = async () => {
-    // Write your code here 
+    process.stdout.write('Hello! Please enter any data: ')
+    readLine.on('line', (data) => {
+        myWriteStream.write(data + '\n', err => {
+            if (err) throw new Error('Something wrong!');
+        })
+    })
 };
+
+write();
