a1a3d0f8a6032aa70190340ad41b1a7195080b83
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-02 21:17:07 +0300

    feat: add cli-env solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
-    // Write your code here 
+    Object.entries(process.env).map((elem) => {
+        if (!elem[0].indexOf("RSS_")) {
+            console.log(`${elem[0]}=${elem[1]}`);
+        }
+    });
 };
+
+parseEnv();
