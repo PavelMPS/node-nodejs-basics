c909d8ca651a0a4d106df29f2400918ec60d6109
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-03 22:59:18 +0300

    feat: add worker-treads solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,24 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cores = os.cpus().length;
+    const promises = [];
+    for (let i = 0; i < cores; i++ ) {
+        promises[i] = new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', {
+                workerData: 10 + i,
+            });
+            worker.on('message', msg => {
+                resolve(msg);
+            });
+            worker.on('error', msg => {
+                reject(msg);
+            });
+        });
+    }
+    const result = await Promise.all(promises);
+    console.log(result)
 };
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,18 @@
-// n should be received from main thread
+import { workerData, parentPort } from 'worker_threads';
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    const result = nthFibonacci(workerData)
+    const sendToParent = {
+        status: 'error',
+        data: null,
+    }
+    if(result) {
+        sendToParent.status = 'resolved';
+        sendToParent.data = result;   
+    }
+    parentPort.postMessage(sendToParent);
 };
+
+sendResult();
