4502af7f546e49595aa3d314dc37c8457d1cdfa2
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-03 11:50:01 +0300

    feat: add zip-decompress solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,23 @@
+import * as path from "path";
+import { createGunzip } from "zlib";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const gunzip = createGunzip();
+const source = createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+const destination = createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+
 export const decompress = async () => {
-    // Write your code here 
+     pipeline(source, gunzip, destination, err => {
+         if(err) {
+            process.exitCode = 1;
+            throw new Error('FS operation failed');
+         }
+     })
 };
+
+decompress();
