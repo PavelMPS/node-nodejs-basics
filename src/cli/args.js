11de1a0b5254159d2b42f6edd609e60c5875034a
Pavel <87997317+PavelMPS@users.noreply.github.com>
2022-06-05 18:03:26 +0300

    feat: add instructions for inspectors

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,10 +1,16 @@
 export const parseArgs = () => {
     const args = process.argv.slice(2);
+    let result = '';
     args.forEach((arg, index) => {
         if(index % 2 === 0) {
-            console.log(arg.slice(2) + ' is ' + args[index + 1]);
+           result += `${arg.slice(2)} is ${args[index + 1]}, `;
         }
-    })
+    });
+    console.log(result.slice(0, -2));
 };
 
 parseArgs();
+
+//Для проверки наберите в консоли:
+//Git Bash:
+//node args --propName value --prop2Name value2
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,9 +1,14 @@
 export const parseEnv = () => {
+    let result = '';
     Object.entries(process.env).map((elem) => {
-        if (!elem[0].indexOf("RSS_")) {
-            console.log(`${elem[0]}=${elem[1]}`);
+        if (elem[0].indexOf("RSS_") === 0) {
+           result +=`${elem[0]}=${elem[1]}; `
         }
     });
+    console.log(result.slice(0, -2));
 };
 
 parseEnv();
+
+//Для проверки наберите в консоли:
+//RSS_name1=value1 RSS_name2=value2 node env
